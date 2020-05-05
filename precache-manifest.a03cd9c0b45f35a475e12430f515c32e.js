c875940740721b5f9bfa987d3bdd738a60030c16
paco.gordillo <paco.gordillo@best.eu.org>
2020-05-05 23:02:02 +0300

    deploy

---
+++ precache-manifest.a03cd9c0b45f35a475e12430f515c32e.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fd7883c1f291333346cb",
+    "url": "/css/app.caa7cd9e.css"
+  },
+  {
+    "revision": "48a30ae781f1f8d49703",
+    "url": "/css/chunk-vendors.d64f6a5d.css"
+  },
+  {
+    "revision": "15933f73856202430d383613a25f6437",
+    "url": "/index.html"
+  },
+  {
+    "revision": "fd7883c1f291333346cb",
+    "url": "/js/app.e7560b49.js"
+  },
+  {
+    "revision": "48a30ae781f1f8d49703",
+    "url": "/js/chunk-vendors.4cc076e4.js"
+  },
+  {
+    "revision": "991384478969d6053bb9",
+    "url": "/js/play.b546cb3b.js"
+  },
+  {
+    "revision": "d4cec5802aafc8290a9d6c860802636b",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
